a769b6760e32b1e4aae5a9f783f52fdd72bfd0f9
Myles Bennett <myles.bennett@hotmail.com>
2019-03-28 16:56:42 -0400

    added second .then callback function

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -8,17 +8,22 @@ document.addEventListener("DOMContentLoaded", function() {
     // on click make get request to ping and display response data
     secondButton = document.getElementById('second-button');
     secondButton.addEventListener('click', function() {
-        const request = axios.get('http://intro-ajax-api.herokuapp.com/pong/')
+        const request = axios.get('http://intro-ajax-api.herokuapp.com/ping/')
         .then(function(response){
             console.log(response.data);
-            const dataSection = document.createElement('section');
-            dataSection.innerHTML = response.data;
-            document.body.appendChild(dataSection);
+            const sectionOne = document.createElement('section');
+            sectionOne.innerHTML = response.data;
+            document.body.appendChild(sectionOne);
+            console.log('That went well!');
         })
         .catch(function(error) {
-            sectionFour = document.createElement('section');
-            sectionFour.innerHTML = error.response.data
-            document.body.appendChild(sectionFour);
+            errorSection = document.createElement('section');
+            errorSection.innerHTML = error.response.data
+            document.body.appendChild(errorSection);
+            console.log('That did not go well...');
+        })
+        .then(function() {
+            console.log('Hey, the request finished!');
         })
     })
 });
