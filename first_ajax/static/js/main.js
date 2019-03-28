d960f3bb1ae6125c0472b8c58498d482f433a395
Myles Bennett <myles.bennett@hotmail.com>
2019-03-28 16:10:42 -0400

    added second button that makes get request to ping and displays data

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,6 +1,19 @@
+// on click make get request to api root
 document.addEventListener("DOMContentLoaded", function() {
-    firstButton = document.getElementById('first-button')
+    firstButton = document.getElementById('first-button');
     firstButton.addEventListener('click', function() {
         const request = axios.get('http://intro-ajax-api.herokuapp.com/');
-    })
+    });
+
+    // on click make get request to ping and display response data
+    secondButton = document.getElementById('second-button');
+    secondButton.addEventListener('click', function() {
+        const request = axios.get('http://intro-ajax-api.herokuapp.com/ping/')
+        .then(function(response){
+            console.log(response.data);
+            const dataSection = document.createElement('section');
+            dataSection.innerHTML = response.data;
+            document.body.appendChild(dataSection);
+        });
+    });
 });
