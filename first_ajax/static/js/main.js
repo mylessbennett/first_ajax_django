865da8553e271476dd73ae578c47a1414565b704
Myles Bennett <myles.bennett@hotmail.com>
2019-03-28 16:32:45 -0400

    added catch callback

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -8,12 +8,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // on click make get request to ping and display response data
     secondButton = document.getElementById('second-button');
     secondButton.addEventListener('click', function() {
-        const request = axios.get('http://intro-ajax-api.herokuapp.com/ping/')
+        const request = axios.get('http://intro-ajax-api.herokuapp.com/pong/')
         .then(function(response){
             console.log(response.data);
             const dataSection = document.createElement('section');
             dataSection.innerHTML = response.data;
             document.body.appendChild(dataSection);
-        });
-    });
+        })
+        .catch(function(error) {
+            sectionFour = document.createElement('section');
+            sectionFour.innerHTML = error.response.data
+            document.body.appendChild(sectionFour);
+        })
+    })
 });
