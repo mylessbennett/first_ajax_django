77a4bad65c40c660a31fbd4af8b493b7457592bb
Myles Bennett <myles.bennett@hotmail.com>
2019-03-29 14:43:42 -0400

    finished assignment/ added button to handle ajax request to a car

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -37,4 +37,28 @@ document.addEventListener("DOMContentLoaded", function() {
             document.body.appendChild(sectionTwo);
         })
     });
+
+    // on click make get request to time
+    fourthButton = document.getElementById('fourth-button');
+    fourthButton.addEventListener('click', function() {
+        const request = axios.get('http://intro-ajax-api.herokuapp.com/time/', {
+            params: {timezone: "Europe/Sofia"}
+        })
+        .then(function(response) {
+            const sectionThree = document.createElement('section');
+            sectionThree.innerHTML = response.data;
+            document.body.appendChild(sectionThree);
+        })
+    })
+
+    fifthButton = document.getElementById('fifth-button');
+    fifthButton.addEventListener('click', function() {
+        const request = axios.get('http://intro-ajax-api.herokuapp.com/a_car/')
+        .then(function(response) {
+            const carItem = document.createElement('li');
+            carItem.innerHTML = response.data;
+            console.log('A car!');
+            document.getElementById('car-list').appendChild(carItem);
+        })
+    })
 });
